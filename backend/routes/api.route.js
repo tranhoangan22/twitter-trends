2b669d2cf9640c3601375343613a67d2bcedc6e4
An Tran <hoangan08ece@gmail.com>
2022-01-20 15:40:20 +0100

    finished all features

---
+++ backend/routes/api.route.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+const { response } = require("express");
+const Twitter = require("twitter");
+
+// initialize a new twitter client
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_API_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_API_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_SECRET,
+});
+
+// get trending topics for a particular place
+router.get("/trends", async (req, res, next) => {
+  const id = req.query.woeid;
+
+  try {
+    // https://developer.twitter.com/en/docs/twitter-api/v1/trends/trends-for-location/api-reference/get-trends-place
+    const trends = await client.get("trends/place.json", {
+      id,
+    });
+
+    res.send(trends);
+  } catch (error) {
+    next(error); // app.js already handles the error
+  }
+});
+
+// get the WOEID for a particular location (lat, long)
+router.get("/near-me", async (req, res, next) => {
+  try {
+    const { lat, long } = req.query;
+
+    // https://developer.twitter.com/en/docs/twitter-api/v1/trends/locations-with-trending-topics/api-reference/get-trends-closest
+    const response = await client.get("/trends/closest.json", {
+      lat,
+      long,
+    });
+
+    res.send(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
+
+/*
+
+Twitter API endpoints: https://developer.twitter.com/en/docs/api-reference-index#twitter-api-standard
+
+`GET trends/place`: https://developer.twitter.com/en/docs/twitter-api/v1/trends/trends-for-location/api-reference/get-trends-place
+
+*/
---
+++ frontend/src/App.js
@@ -0,0 +1,91 @@
+import logo from "./twitter.svg";
+import "./App.css";
+import { FaCrosshairs } from "react-icons/fa";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [trends, setTrends] = useState([]);
+  const [woeid, setWoeid] = useState("1");
+
+  useEffect(() => getTrends(), [woeid]);
+
+  // make API call to our backend
+  const getTrends = () => {
+    axios
+      .get("/api/trends", {
+        params: {
+          woeid,
+        },
+      }) // request will be proxy-ed to the server on port 4000 (defined in package.json)
+      .then((response) => {
+        // console.log(response.data[0].trends);
+        setTrends(response.data[0].trends);
+      })
+      .catch((error) => console.log(error.message));
+  };
+
+  const handleLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          axios
+            .get("/api/near-me", {
+              params: {
+                lat: position.coords.latitude,
+                long: position.coords.longitude,
+              },
+            })
+            .then((response) => {
+              setWoeid(response.data[0].woeid);
+            })
+            .catch((error) => console.log(error.message));
+        },
+        (error) => console.log(error)
+      );
+    } else {
+      alert("location not supported");
+    }
+  };
+
+  const listTrends = () => {
+    return (
+      <ul>
+        {trends.map((trend, index) => (
+          <li key={index}>
+            <a href={trend.url}>{trend.name}</a>
+            {trend.tweet_volume && (
+              <span className="tweet_volume">{trend.tweet_volume}</span>
+            )}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="logo" alt="twitter" />
+        <h3>Twitter Trends</h3>
+      </header>
+      <div className="menu">
+        <select
+          name="trending-place"
+          id=""
+          onChange={(event) => setWoeid(event.target.value)} // as soon as `woeid` changes, the function inside useEffect gets fired and then `trends` will be updated => UI is populated with corresponding trends
+        >
+          <option value="1">Worldwide</option>
+          <option value="23424829">Germany</option>
+          <option value="23424977">United States</option>
+        </select>
+        <div className="location" onClick={handleLocation}>
+          <FaCrosshairs />
+        </div>
+      </div>
+      <div className="content">{listTrends()}</div>
+    </div>
+  );
+};
+
+export default App;
