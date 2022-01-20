25b64bbaa51fdaa22f51fcdfd4e5ba72dadd4536
An Tran <hoangan08ece@gmail.com>
2022-01-20 16:27:47 +0100

    debugging deployVersion

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 require("dotenv").config();
 // const cors = require("cors");
 const enforce = require("express-sslify"); // used to enforces HTTPS connections on any incoming GET and HEAD requests
+const path = require("path");
 const compression = require("compression");
 
 const app = express();
